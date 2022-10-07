c17bc35e66ce0566af551b56aa0ff17fa3510c4b
Pedro Cruz <68717923+Pulizs@users.noreply.github.com>
2022-10-07 00:04:00 -0300

    Add files via upload

---
+++ trabalho-curriculo-Web-main/página/backend/script.js
@@ -0,0 +1,19 @@
+// Capturando os campos da página Login
+const myUserName = document.getElementById("username");
+const myPassword = document.getElementById("password");
+
+// Quando o botão Entrar é clicado
+function isValidLogin() {
+
+  // Buscando no sessionStorage o nome e a senha registrados na tela de SignUp.
+  let registeredUsername = sessionStorage.getItem('userName');
+  let registeredPassword = sessionStorage.getItem('password');  
+
+  // Se usuário e senha digitados são iguais aos registrados no sessionStorage
+  if (myUserName.value == registeredUsername && myPassword.value == registeredPassword) {
+    // Carrega a página list
+    window.location.href = "../list/list.html";
+  } else {
+    alert("Usuário e/ou senha inválidos");
+  }
+}
